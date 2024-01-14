026a2ebf6f76977527cecbca9298a41377bdee2f
Jigmet Dadul <85403349+jigmetdadul@users.noreply.github.com>
2024-01-14 20:07:13 +0530

    .

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,22 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var showAler = false
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    @State var correctAnswer = Int.random(in: 0...6)
+    @State var showScore = false
+    @State var scoreTitle = "title"
+    
     var body: some View {
-        VStack{
-            Button("Aler"){
-                showAler = true
-            }.alert("Alert", isPresented: $showAler){
-                Button("Cancel"){}
-                Button("Continue"){}
-            } message: {
-                Button("hi", systemImage: "pencil"){
-                    
+        ZStack{
+            Color.blue.ignoresSafeArea()
+            VStack(spacing: 30){
+                VStack{
+                    Text("Tap the flag of: ").foregroundStyle(Color(.white))
+                    Text(countries[correctAnswer]).foregroundStyle(Color(.white))
+                }
+                
+                ForEach(0..<3){ number in
+                    Text("Before: \(number)")
+                    Button{
+                        flagTapped(number)
+                        //Text("after: \(number)")
+                    }label: {
+                        Image(countries[number])
+                    }.buttonStyle(.bordered).foregroundColor(.white).tint(.red).padding(1)
                 }
             }
-            Text(String(showAler))
+        }.alert(scoreTitle, isPresented: $showScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("\(correctAnswer)")
         }
+        }
+    
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...6)
     }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showScore = true
+    }
+  
+    
+    
 }
 
 #Preview {
